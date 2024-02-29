d34d59a4e64438b658f95fba4a751c52c3b5b035
KishalanPather <119253561+KishalanPather@users.noreply.github.com>
2024-02-29 20:35:31 +0200

    abdul's update to Client.py

--- Client.py
+++ Client.py
@@ -1,75 +1,102 @@
 from socket import *
 import threading
+import os
 
-serverName = "192.168.0.125"
+serverName = "192.168.56.1"
 serverPort = 12000
-name = "john"        # our own name
-ourPort = 13000
-listOfContacts = []
+name = "aj"        # our own name
+ourIP = "192.168.56.1"
+ourPort = 15000     # must be unique if you are running multiple cients on the same machine
+ipAndPortOfSomeContact = []     # when the server send the ip and port of a person we are trying to message it gets stored here
+allContactsMessaged = []  # there names ip and port all stored in this order
 
+privacyStatus = input("Would you like to be private or public on the server?\n")
+privacyStatus = privacyStatus.lower()
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+clientSocket.connect((serverName, serverPort))      # just making tcp connection to server
 
-serverResponse = clientSocket.recv(1024).decode()
-if(serverResponse == "NAME&PORT"):
-    clientSocket.send((name + "," + str(ourPort)).encode())                # send the server our name so it can store it in its list of clients
-
+serverResponse = clientSocket.recv(1024).decode()       # server will start the communication by 
+if(serverResponse == "NAME&PORT&PRIVACYSTATUS"):
+    clientSocket.send((name + "," + str(ourPort) + "," + privacyStatus).encode())                # send the server our name so it can store it in its list of clients
 
 def receiveIncomingMessagesFromServer(clientSocket):
-    while True:
-        print("the code is entering this section")
-        incomingMessage = clientSocket.recv(1024).decode()
-        print("this piece of code is running!!!")
-        if(incomingMessage == ""):
-            continue
-        if(incomingMessage[:6] == "123123"):
-            listOfContacts.append(incomingMessage[7:])
-        else:
-            print(incomingMessage)
-        incomingMessage = ""
-            
-
+    try:
+        while True:
+            incomingMessage = clientSocket.recv(1024).decode()
+            if(incomingMessage[:6] == "123123"):
+                print(incomingMessage[6:])
+            else:   
+                ipAndPortOfSomeContact.append(incomingMessage)
+    except ConnectionAbortedError:
+        pass
  
 def messageContact(ipAddress, port, message):
-    privateSocket = socket(AF_INET, SOCK_STREAM)
-    privateSocket.connect((ipAddress, port))
-    privateSocket.send((name + ": " + message).encode())
-    privateSocket.close()
+    privateUDPSocket = socket(AF_INET, SOCK_DGRAM)
+    privateUDPSocket.sendto((name + ": " + message).encode(), (ipAddress, int(port)))
+    privateUDPSocket.close()
 
 # we also need a thread which is constantly running and accepting connection requests from other clients when they private message us
 def acceptConnectionRequests():
-    acceptConnectionSocket = socket(AF_INET, SOCK_STREAM)
-    acceptConnectionSocket.bind(("192.168.0.125", ourPort))
-    acceptConnectionSocket.listen(3)
+    acceptConnectionSocket = socket(AF_INET, SOCK_DGRAM)
+    acceptConnectionSocket.bind((ourIP, ourPort))
     while True:
-        client, addr = acceptConnectionSocket.accept()
-        print(client.recv(1024).decode())  # this would be incoming messages from other contacts     
+        message, addr = acceptConnectionSocket.recvfrom(1024)
+        print(message.decode())  # this would be incoming messages from other contacts     
         
-thready = threading.Thread(target = receiveIncomingMessagesFromServer, args=(clientSocket,))
+thready = threading.Thread(target= receiveIncomingMessagesFromServer, args = (clientSocket,))        
 thready.start()
 threadx = threading.Thread(target=acceptConnectionRequests, )
 threadx.start()
-response = input("Would you like to send a Group Message(G) or a Private Message(P)?\n")
-while(response != "EXIT"):
-    if(response == "G"):
-        response = input("What would you like to send?\n")
-        clientSocket.send(("G," + response).encode())
-    elif(response == "P"):
-        recipient = input("Who would you like to send a private message to?\n")
-        response = input("What would you like to send?\n")
-        clientSocket.send((recipient + "," + response).encode())
-        # server must respond by sending us the IP address and the port of the contact we are trying to message and then we make a connection to them
-        #theIPAndPort = listOfContacts[0]
-        #parts = theIPAndPort.split(",")
-        theIP =  "192.168.0.125"  # parts[0].split("(")[1].strip("'")
-        thePort =  14000  # parts[2]
-        #listOfContacts.clear()
-        # now create a new thread to send a private message to this contact we are trying to message
-        thread = threading.Thread(target = messageContact, args = (theIP, thePort, response,))
-        thread.start()
-        thread.join()
-    response = input("Would you like to send a Group Message(G) or a Private Message(P)?\n")
-        
-    
-clientSocket.close()
+response = input("Would you like to send a Group Message(G) or a Private Message(P) or a List of all Clients available(L)?\n")
+try:
+    while(response != "EXIT"):
+        if(response == "G"):
+            response = input("What would you like to send?\n")
+            clientSocket.send(("G," + name + ": " + response).encode())
+        elif(response == "P"):
+            recipient = input("Who would you like to send a private message to?\n")
+            response = input("What would you like to send?\n")
+            found = False
+            for items in allContactsMessaged:
+                if(items[0] == recipient):
+                    ipAndPortOfSomeContact.append(items[1] + "," + items[2])
+                    found = True
+                    break
+            if(found == False):
+                clientSocket.send(("P," + recipient + "," + response).encode())
+            # server must respond by sending us the IP address and the port of the contact we are trying to message and then we make a connection to them
+
+            while(len(ipAndPortOfSomeContact) < 1):
+                # basically wait till the message arrives from server and is put in the list of contacts list
+                pass       
+            incomingMessage = ipAndPortOfSomeContact[0]
+            ipAndPortOfSomeContact.clear()
+            if(incomingMessage != "Error"):
+                parts = incomingMessage.split(",")
+                theIP =  parts[0]
+                thePort =  parts[1]
+                allContactsMessaged.append([recipient, theIP, thePort])
+
+            # now create a new thread to send a private message to this contact we are trying to message
+                thread = threading.Thread(target = messageContact, args = (theIP, thePort,  response,))
+                thread.start()
+                thread.join()
+            else:
+                print("The client does not exist or is set to private.\n")
+        elif(response == "L"):
+            clientSocket.send(("L, ").encode())
+            while(len(ipAndPortOfSomeContact) < 1):
+                # basically wait till the message arrives from server and is put in the list of contacts list
+                pass  
+            incomingList = ipAndPortOfSomeContact[0]
+            ipAndPortOfSomeContact.clear()
+            print("The following clients are available: ")
+            print(incomingList)
+        response = input("Would you like to send a Group Message(G) or a Private Message(P) or a List of all Clients available(L)?\n")
+    clientSocket.close()
+    os._exit(0)
+except ConnectionAbortedError:
+    pass
+
+
